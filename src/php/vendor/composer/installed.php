b02b7bb96ffc8fef0359772285f0de4bdacb07c0
Alper Kum <142728187+alp3rkum@users.noreply.github.com>
2026-08-11 15:30:27 +0300

    Global hook attempt #3 on local machine

--- src/php/vendor/composer/installed.php
+++ src/php/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => '2.x-dev',
         'version' => '2.9999999.9999999.9999999-dev',
-        'reference' => '662494adb156e2747d4ee79d226dfb86af586eba',
+        'reference' => 'cc030f96e44c2979405d16dc169e6e3d935ceb76',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => '2.x-dev',
             'version' => '2.9999999.9999999.9999999-dev',
-            'reference' => '662494adb156e2747d4ee79d226dfb86af586eba',
+            'reference' => 'cc030f96e44c2979405d16dc169e6e3d935ceb76',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
